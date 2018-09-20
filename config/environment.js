de92594cec9f9dbc5d6dc2582dd50e515f418640
Caoimhe Clarke <caoimhepower@gmail.com>
2018-09-20 09:34:53 +0100

    Set up for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 4000;
-const dbURI = 'mongodb://localhost:27017/blog-posts';
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/blog-posts';
 
 module.exports = { port, dbURI };
